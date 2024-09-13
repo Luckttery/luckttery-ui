da4e88fe6a7edae0acd95683300e1e6c36f9a0d8
Jwirang <haha71119@gamil.com>
2024-09-13 23:36:07 +0900

    refactor: / 접두사 추가

--- src/api/lucktteryApi/api.ts
+++ src/api/lucktteryApi/api.ts
@@ -3,7 +3,7 @@ import { ResponsNumber } from "./types"
 
 export const fetchGetNumber = async (number: number, median: number): Promise<ResponsNumber> => {
   try {
-    const response = await lucktteryClient.get(`lotto/recommend?set_count=${number}&median_frequency_percentage=${median}`)
+    const response = await lucktteryClient.get(`/lotto/recommend?set_count=${number}&median_frequency_percentage=${median}`)
     return response.data
   } catch (error: unknown) {
     throw error
