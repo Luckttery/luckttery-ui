e5102108690ba365f487eecd76d7207016bc97e4
Jwirang <haha71119@gamil.com>
2024-09-16 22:07:12 +0900

    feat: api 엔드포인트 수정

--- src/api/lucktteryApi/api.ts
+++ src/api/lucktteryApi/api.ts
@@ -1,9 +1,10 @@
 import lucktteryClient from "./client"
 import { ResponsNumber } from "./types"
 
-export const fetchGetNumber = async (number: number, median: number): Promise<ResponsNumber> => {
+export const fetchGetNumber = async (number: number, lower: number, upper: number): Promise<ResponsNumber> => {
   try {
-    const response = await lucktteryClient.get(`/lotto/recommend?set_count=${number}&median_frequency_percentage=${median}`)
+    console.log(lower, upper)
+    const response = await lucktteryClient.get(`/lotto/recommend?set_count=${number}&lower_percent=${lower}&upper_percent=${upper}`)
     return response.data
   } catch (error: unknown) {
     throw error
