fc5dde4e5bcd6f0f648778a21116dd51d10ef7f0
Jwirang <haha71119@gamil.com>
2024-09-17 22:28:28 +0900

    feat: 로그 삭제

--- src/api/lucktteryApi/api.ts
+++ src/api/lucktteryApi/api.ts
@@ -3,7 +3,6 @@ import { ResponsNumber } from "./types"
 
 export const fetchGetNumber = async (number: number, lower: number, upper: number): Promise<ResponsNumber> => {
   try {
-    console.log(lower, upper)
     const response = await lucktteryClient.get(`/lotto/recommend?set_count=${number}&lower_percent=${lower}&upper_percent=${upper}`)
     return response.data
   } catch (error: unknown) {
