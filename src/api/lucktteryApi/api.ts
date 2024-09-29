dca9b55305e3d98f8b96097ee73048602b2d37b1
Jwirang <haha71119@gamil.com>
2024-09-29 18:34:09 +0900

    feat: 전 회차 조회 api 코드 작성

--- src/api/lucktteryApi/api.ts
+++ src/api/lucktteryApi/api.ts
@@ -1,9 +1,18 @@
 import lucktteryClient from "./client"
-import { ResponsNumber } from "./types"
+import { LottoDrawResponse, ResponseNumber } from "./types"
 
-export const fetchGetNumber = async (number: number, lower: number, upper: number): Promise<ResponsNumber> => {
+export const fetchGetNumber = async (number: number, lower: number, upper: number): Promise<ResponseNumber> => {
   try {
     const response = await lucktteryClient.get(`/lotto/recommend?set_count=${number}&lower_percent=${lower}&upper_percent=${upper}`)
+    return response.data
+  } catch (error: unknown) {
+    throw error
+  }
+}
+
+export const fetchLatest = async (): Promise<LottoDrawResponse> => {
+  try {
+    const response = await lucktteryClient.get('/lotto/draws/latest')
     return response.data
   } catch (error: unknown) {
     throw error
--- src/api/lucktteryApi/types/index.ts
+++ src/api/lucktteryApi/types/index.ts
@@ -1,3 +1,21 @@
-export type ResponsNumber = {
+export type ResponseNumber = {
   sets: number[][]
+}
+
+export type LottoDrawResponse = {
+  _id: string
+  draw: number
+  date: Date
+  first_prize: PrizeResponse
+  second_prize: PrizeResponse
+  third_prize: PrizeResponse
+  fourth_prize: PrizeResponse
+  fifth_prize: PrizeResponse
+  numbers: number[]
+  bonus_number: number
+}
+
+export type PrizeResponse = {
+  winners: number
+  amount: number
 }
