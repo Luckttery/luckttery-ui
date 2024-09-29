6d5bf0d02a1c1cc2aa802865096fb8cf286e61ba
Jwirang <haha71119@gamil.com>
2024-09-29 23:02:50 +0900

    feat: 전 회차 정보 컴포넌트로 작업

---
+++ src/components/latest/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const StyledNumbersContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const DrawHighlight = styled.span`
+  color: #FF6868;
+  font-weight: bold;
+`;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from "react";
 import { StyledContainer} from "./style";
-import LottoNumber from "../../components/number";
-import { fetchLatest } from "../../api/lucktteryApi/api";
 import Header from "../../components/Header";
-import { LottoDrawResponse } from "../../api/lucktteryApi/types";
-import { Circle } from "../../components/number/style";
 import Recommend from "../../components/recommend";
+import Latest from "../../components/latest";
 
 const Home = () => {
-  const [drawdata, setDrawdata] = useState<LottoDrawResponse>()
-
-  useEffect(() => {
-    const getdrawData = async () => {
-      await getLottoDrawData() 
-    }
-    getdrawData()
-  }, [])
-
-  const getLottoDrawData = async () => {
-    try {
-      const result = await fetchLatest()
-      setDrawdata(result)
-    } catch (error){
-      alert(error)
-    }
-  }
-
   return (
     <StyledContainer>
       <Header/>
       <Recommend/>
-
-      <h3>{drawdata?.draw}회 당첨 번호</h3>
-      <LottoNumber numbers={drawdata?.numbers}></LottoNumber>
-      <h2>+</h2>
-      <Circle key={1} $number={drawdata?.bonus_number}>
-          {drawdata?.bonus_number}
-      </Circle>
+      <Latest/>
     </StyledContainer>
   );
 };
