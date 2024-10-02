cde107fd5bb61135817a41a55a7e0fc641f747eb
Jwirang <haha71119@gamil.com>
2024-10-02 10:21:42 +0900

    feat: 스킬레톤 로딩 적용

--- src/components/latest/index.tsx
+++ src/components/latest/index.tsx
@@ -4,14 +4,16 @@ import LottoNumber from "../number";
 import { DrawHighlight, StyledContainer, StyledNumbersContainer } from "./style";
 import { LottoDrawResponse } from "../../api/lucktteryApi/types";
 import { Circle } from "../number/style";
+import Skeleton from "../skeleton";
 
 const Latest = () => {
-  const [drawdata, setDrawdata] = useState<LottoDrawResponse>()
+  const [drawdata, setDrawdata] = useState<LottoDrawResponse | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getdrawData = async () => {
       await getLottoDrawData()
-    };
+    }
     getdrawData()
   }, [])
 
@@ -21,22 +23,38 @@ const Latest = () => {
       setDrawdata(result)
     } catch (error) {
       alert(error)
+    } finally {
+      setLoading(false)
     }
   }
 
   return (
     <StyledContainer>
-      <h2>
-        <DrawHighlight>{drawdata?.draw}회 </DrawHighlight>
-        당첨 번호
-      </h2>
-      <StyledNumbersContainer>
-        <LottoNumber numbers={drawdata?.numbers}></LottoNumber>
-        <h2>+</h2>
-        <Circle key={1} $number={drawdata?.bonus_number}>{drawdata?.bonus_number}</Circle>
-      </StyledNumbersContainer>
+      {loading ? (
+        <Skeleton height = '50px' width="190px"/>
+      ) : (
+        <>
+          <h2>
+            <DrawHighlight>{drawdata?.draw}회 </DrawHighlight>
+            당첨 번호
+          </h2>
+          </>
+          )}
+          {loading ? (
+            <Skeleton height = '80px' width="500px"/>
+          ) : (
+            <>
+          <StyledNumbersContainer>
+            <LottoNumber numbers={drawdata?.numbers} />
+            <h2>+</h2>
+            <Circle key={1} $number={drawdata?.bonus_number}>
+              {drawdata?.bonus_number}
+            </Circle>
+          </StyledNumbersContainer>
+          </>
+          )}
     </StyledContainer>
-  )
-}
+  );
+};
 
 export default Latest
