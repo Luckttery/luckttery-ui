1435db95f3ad158742c457d6488a69f393d454df
Jwirang <haha71119@gamil.com>
2024-10-14 11:36:05 +0900

    feat: api 코드 작성

--- src/api/lucktteryApi/api.ts
+++ src/api/lucktteryApi/api.ts
@@ -1,5 +1,5 @@
 import lucktteryClient from "./client"
-import { LottoDrawResponse, ResponseNumber } from "./types"
+import { LottoDrawResponse, ResponseNumber, StoreInfo } from "./types"
 
 export const fetchGetNumber = async (number: number, lower: number, upper: number): Promise<ResponseNumber> => {
   try {
@@ -13,6 +13,15 @@ export const fetchGetNumber = async (number: number, lower: number, upper: numbe
 export const fetchLatest = async (): Promise<LottoDrawResponse> => {
   try {
     const response = await lucktteryClient.get('/lotto/draws/latest')
+    return response.data
+  } catch (error: unknown) {
+    throw error
+  }
+}
+
+export const fetchStoreList = async (latitude: number, longitude: number, radius: number): Promise<StoreInfo[]> => {
+  try {
+    const response = await lucktteryClient.get(`/retail-store/nearby?latitude=${latitude}&longitude=${longitude}&radius=${radius}`)
     return response.data
   } catch (error: unknown) {
     throw error
--- src/api/lucktteryApi/types/index.ts
+++ src/api/lucktteryApi/types/index.ts
@@ -19,3 +19,33 @@ export type PrizeResponse = {
   winners: number
   amount: number
 }
+
+export type StoreInfo = {
+  _id: string
+  address: Storeaddress
+  location: StoreLocation
+  name: string
+  phone_number: string | null
+  retailer_id: string
+  selling_items: StoreSelling_items
+}
+
+export type Storeaddress = {
+  additional_info: string
+  district: string
+  land_lot_number: string
+  region: string
+  road_name_address: string
+  town: string
+}
+
+export type StoreLocation = {
+  type: string
+  coordinates: number[]
+}
+
+export type StoreSelling_items = {
+  sells_lotto_520: boolean
+  sells_lotto_645: boolean
+  sells_speeto: boolean
+}
