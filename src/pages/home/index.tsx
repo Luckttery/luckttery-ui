e0d04742fcabf539dc42ebaac57e689dd2da5c19
Jwirang <haha71119@gamil.com>
2024-10-15 15:48:02 +0900

    feat: home 화면에 지도 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
 import { StyledContainer} from "./style";
 import Recommend from "../../components/recommend";
 import Latest from "../../components/latest";
+import Map from "../../components/NaverMap"
 
 const Home = () => {
   return (
     <StyledContainer>
       <Recommend/>
       <Latest/>
+      <Map/>
     </StyledContainer>
   );
 };
