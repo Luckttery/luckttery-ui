7136d4cb7e2c93c8cc0dea072a1c40903ec7a6dc
jeyog <choijy8816@gmail.com>
2025-01-26 01:37:51 +0900

    feat: 페이지 메타 정보 추가

--- app/routes/draws/route.tsx
+++ app/routes/draws/route.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
 import { dehydrate, HydrationBoundary, InfiniteData, QueryClient, QueryKey } from "@tanstack/react-query";
 import { fetchDraws } from "~/api/lucktteryApi/api";
 import { CursorPage, LottoDrawResponse } from "~/api/lucktteryApi/types";
@@ -6,6 +6,19 @@ import LottoDrawList from "~/components/LottoDrawList";
 import styles from "./styles.module.scss";
 
 const LIMIT = 50;
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Luckttery | 로또 당첨번호 모아보기" },
+    { name: "description", content: "로또 회차별 당첨번호를 한 곳에서 조회해보세요." },
+    { name: "keywords", content: "Luckttery, 로또, 로또645, 로또6/45, 6/45 로또, 로또 당첨번호, 로또 화차별 당첨번호" },
+    { property: "og:title", content: "Luckttery | 로또 당첨번호 모아보기"},
+    { property: "og:description", content: "로또 회차별 당첨번호를 한 곳에서 조회해보세요."},
+    { property: "og:type", content: "website"},
+    { property: "og:url", content: "https://luckttery.com" },
+    { property: "og:image", content: "https://luckttery.com/android-chrome-512x512.png" }
+  ];
+};
 
 export const loader = async () => {
   const queryClient = new QueryClient()
@@ -32,7 +45,7 @@ export default function Index() {
 
   return (
     <div className={styles.container}>
-      <h1>로또 당첨 결과</h1>
+      <h1>로또 당첨번호 모아보기</h1>
       <HydrationBoundary state={dehydratedState}>
         <LottoDrawList limit={LIMIT} />
       </HydrationBoundary>
