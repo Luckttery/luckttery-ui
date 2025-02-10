f0bf4b1b548e0c60af83cdb01eb4467026f0e6a0
jeyog <choijy8816@gmail.com>
2025-02-10 21:02:48 +0900

    chore: 판매 품목 아이콘 이미지 변경

--- app/components/NearByStoresMap/StoreInfoWindow/index.tsx
+++ app/components/NearByStoresMap/StoreInfoWindow/index.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 import styles from "./styles.module.scss";
 import { StoreSelling_items } from "~/api/lucktteryApi/types";
-import IcoSeller645 from "~/assets/ico_seller_645.png";
-import IcoSeller720 from "~/assets/ico_seller_720.png";
-import IcoSellerSpeetto from "~/assets/ico_seller_speetto.png";
+import Ico645 from "~/assets/ico_645.png";
+import Ico720 from "~/assets/ico_720.png";
+import IcoSpeetto from "~/assets/ico_speetto.png";
 
 type StoreInfoWindowProps = {
   name: string
@@ -20,14 +20,14 @@ export const StoreInfoWindow: React.FC<StoreInfoWindowProps> = ({ name, address,
 
   const sellerIcons: string[] = []
 
-  if (sellingItems.sells_lotto) sellerIcons.push(IcoSeller645);
-  if (sellingItems.sells_annuity) sellerIcons.push(IcoSeller720);
+  if (sellingItems.sells_lotto) sellerIcons.push(Ico645);
+  if (sellingItems.sells_annuity) sellerIcons.push(Ico720);
   if (
     sellingItems.sells_speeto_500 ||
     sellingItems.sells_speeto_1000 ||
     sellingItems.sells_speeto_2000
   ) {
-    sellerIcons.push(IcoSellerSpeetto);
+    sellerIcons.push(IcoSpeetto);
   }
 
   return (
@@ -40,7 +40,7 @@ export const StoreInfoWindow: React.FC<StoreInfoWindowProps> = ({ name, address,
       </div>
       <div>
         {sellerIcons.map((icon, index) => (
-          <img alt={`item-${index}`} src={icon} key={index} />
+          <img alt={`item-${index}`} src={icon} height={20} key={index} />
         ))}
       </div>
     </div>
