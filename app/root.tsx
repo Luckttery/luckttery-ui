21fef6391af8c667d8a8c45abb6f20384fc41902
jeyog <choijy8816@gmail.com>
2025-12-12 20:47:38 +0900

    chore: 구글 애드 센스 스크립트 추가

--- app/root.tsx
+++ app/root.tsx
@@ -53,6 +53,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1293594318302254" crossOrigin="anonymous"></script>
         <Meta />
         <Links />
       </head>
