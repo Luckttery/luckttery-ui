83a57a1ea3ade41be0a2b1319b68ff9483df84c7
jeyog <choijy8816@gmail.com>
2025-12-13 19:15:58 +0900

    feat: add `useLocation` and `key` prop with pathname to GoogleAdSense components in sidebar and bottom ads.

--- app/components/DesktopSidebarAds/index.tsx
+++ app/components/DesktopSidebarAds/index.tsx
@@ -1,12 +1,16 @@
+import { useLocation } from "@remix-run/react";
 import GoogleAdSense from "../GoogleAdSense";
 import styles from "./styles.module.scss";
 
 export default function DesktopSidebarAds() {
+  const location = useLocation();
+
   return (
     <>
       <aside className={styles.leftSidebar}>
         {/* Desktop_Left_Sidebar */}
         <GoogleAdSense
+          key={`left-ad-${location.pathname}`}
           slot="2449601108"
           format="auto"
           responsive="true"
@@ -17,6 +21,7 @@ export default function DesktopSidebarAds() {
       <aside className={styles.rightSidebar}>
         {/* Desktop_Right_Multiplex */}
         <GoogleAdSense
+          key={`right-ad-${location.pathname}`}
           slot="4279711614"
           format="autorelaxed"
           responsive="true"
